62ddfc1ca30ae3636d3c769b9208526416c767b3
Raul Escobar <raulescobar13@gmail.com>
2017-01-22 14:28:43 -0300

    Writes our own test_helper file

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,38 +1,39 @@
+import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
 import uuid from 'uuid';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
 
-chaiJquery(chai, chai.util, $);
+const $ = _$(global.window);
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+function renderComponent(ComponentClass, props, state) {
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(reducers,state)}>
+            <ComponentClass {...props} />
+        </Provider>
+    );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+    return $(ReactDOM.findDOMNode(componentInstance));
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
+$.fn.simulate = function(eventName, value){
+    if (value) {
+        this.val(value);
+    }
+
+    TestUtils.Simulate[eventName](this[0]);
 };
+
+chaiJquery(chai, chai.util, $);
 
 function createDummy(name = '') {
     return {
